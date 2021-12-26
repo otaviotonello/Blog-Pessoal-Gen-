d65dabce86396bfd48b12715fec2193e18e117d2
Otavio Tonello <otaviotonello0102@gmail.com>
2021-12-26 19:06:17 -0300

    Aprimoramento em Security

--- src/main/java/org/generation/BlogPessoal/security/UserDetailsImplements.java
+++ src/main/java/org/generation/BlogPessoal/security/UserDetailsImplements.java
@@ -16,44 +16,49 @@ public class UserDetailsImplements implements UserDetails {
 	private List<GrantedAuthority> authorities;
 
 	public UserDetailsImplements(Usuario user) {
-		this.userName = user.getUsuario();
-		this.password = user.getSenha();		
+		this.userName= user.getUsuario();
+		this.password = user.getSenha();
 	}
-
-	public UserDetailsImplements() {}
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
 		return authorities;
 	}
 
 	@Override
 	public String getPassword() {
+		// TODO Auto-generated method stub
 		return password;
 	}
 
 	@Override
 	public String getUsername() {
+		// TODO Auto-generated method stub
 		return userName;
 	}
 
 	@Override
 	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public boolean isEnabled() {
+		// TODO Auto-generated method stub
 		return true;
 	}
 } 
